19ae6d34e1ed505b3a1f38e69e014ff7f764e035
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-02 19:20:15 +0530

    initiol two route configured and postMessage schema module configured

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,5 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
 import PostMessage from '../models/postMessage.js';
 
-const router = express.Router();
 
 
 export const getPosts = async (req, res) => {
